ab49446e72caa944044509e5c883f97927bbb749
周煊令 <8882820+zhou-xuanling@user.noreply.gitee.com>
2022-03-04 11:16:43 +0800

    环境设置

--- src/api/http.js
+++ src/api/http.js
@@ -1,7 +1,8 @@
+import env from "./dev";
 // export  const  baseURL='http://10.253.50.180:11504'
 // export  const  baseURL='http://192.168.31.201:11509'
 // export  const  baseURL='http://192.168.0.101:11509'
-export  const  baseURL='http://www.aishuhong.com:11504'
+export  const  baseURL=env.baseUrl
 // export  const  baseURL='http://www.aishuhong.com:11509'
 // export  const  baseURL='http://localhost:12000'
 // export const baseURL='http://172.20.10.5:11504'
